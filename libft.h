fae46ef0504cde49bec01cca4bb75ae6bd441f67
Piriou SCOTT <pscott@student.42.fr>
2018-11-07 11:25:12 +0100

    lots of funct

--- libft.h
+++ libft.h
@@ -3,4 +3,11 @@
 # include <string.h>
 # include <unistd.h>
 # include <stdlib.h>
+void	ft_bzero(void *s, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		isalnum(int c);
+int		isascii(int c);
+int		isprint(int c);
+int		toupper(int c);
 #endif
