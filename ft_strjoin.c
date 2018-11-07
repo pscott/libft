2b2c975af6aad30e1d4274fea6fbc3bb650bb278
Piriou SCOTT <pscott@student.42.fr>
2018-11-07 14:51:58 +0100

    ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pscott <pscott@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 14:46:13 by pscott            #+#    #+#             */
+/*   Updated: 2018/11/07 14:51:49 by pscott           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*r;
+	int		i;
+	int		j;
+
+	if (!(r = (char*)malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		r[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		r[i] = s2[j];
+		i++;
+		j++;
+	}
+	r[i] = 0;
+	return (r);
+}
