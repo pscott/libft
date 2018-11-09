75ca3b140ad41fc794a986cc40ea3af708f2d058
pscott <biliklok@yahoo.fr>
2018-11-09 17:16:28 +0100

    passed unit-tests

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,35 +12,36 @@
 
 #include "libft.h"
 
-static	size_t		trim_len(char const *s)
+static	size_t		right_trim(char const *s)
 {
-        size_t	i;
-        size_t	origin_len;
+		size_t	i;
+		size_t	origin_len;
 
-        origin_len = ft_strlen(s);
-        i = 0;
-        while (s[origin_len - i - 1] == '\t' || s[origin_len - i - 1] == ' '
-                        || s[origin_len - i - 1] == '\n')
-                i++;
-        return (origin_len - i - 1);
+		if ((origin_len = ft_strlen(s)))
+		{
+				i = 0;
+				while (s[origin_len - i - 1] == '\t' || s[origin_len - i - 1] == ' '
+								|| s[origin_len - i - 1] == '\n')
+						i++;
+				return (origin_len - i);
+		}
+		return (0);
 }
 
 char				*ft_strtrim(char const *s)
 {
-        char		*res;
-        size_t		i;
-        size_t		len;
+		char		*res;
+		size_t		i;
+		size_t		len;
 
-        i = 0;
-        if (!s)
-                return (NULL);
-        while (s[i] == '\t' || s[i] == ' ' || s[i] == '\n')
-                i++;
-        len = trim_len(&s[i]);
-        if (len == ft_strlen(s) + 1)
-                return ((char *)s);
-        if (!(res = ft_strnew(len + 1)))
-                return (NULL);
-        ft_strncpy(res, &s[i], len + 1);
-        return (res);
+		i = 0;
+		if (!s)
+				return (NULL);
+		while (s[i] == '\t' || s[i] == ' ' || s[i] == '\n')
+				i++;
+		len = right_trim(&s[i]);
+		if (!(res = ft_strnew(len)))
+				return (NULL);
+		ft_strncpy(res, &s[i], len);
+		return (res);
 }
